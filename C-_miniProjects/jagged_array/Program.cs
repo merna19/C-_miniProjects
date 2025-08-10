a467b7700c43d2112efc6416c7419de4d691c319
Merna Magdy <mernamagdy260@gmail.com>
2025-08-11 00:53:19 +0300

    added array projects

---
+++ C-_miniProjects/jagged_array/Program.cs
@@ -0,0 +1,96 @@
+﻿class MY_CLASS
+{
+    public static void Main()
+    {
+        //create jagged array for grades
+        int[][] grades = new int[5][];
+
+        //declare max number of subjects assigned
+        int subjectMax = 0;
+
+        for (int row = 0; row < 4; row++)
+        {
+            Console.WriteLine($"Student {row + 1}, Please enter the number of subjects:");
+            int subjectNum = int.Parse(Console.ReadLine());
+            grades[row] = new int[subjectNum + 1];
+
+            //find max number of subjects assigned
+            if (subjectNum > subjectMax)
+            {
+                subjectMax = subjectNum;
+            }
+        }
+        //fill grades
+        int grade = 0;
+        for (int row = 0; row < 4; row++)
+        {
+            for (int col = 0; col < grades[row].Length - 1; col++)
+            {
+                grade += 10;
+                grades[row][col] = grade;
+            }
+        }
+        //calculate the students' total
+        for (int row = 0; row < 4; row++)
+        {
+            int total = 0;
+            for (int col = 0; col < grades[row].Length; col++)
+            {
+                total += grades[row][col];
+            }
+            grades[row][grades[row].Length - 1] = total;
+        }
+
+        //calculate AVG for each subject
+        grades[4] = new int[subjectMax];
+        //create students count for each subject
+        int[] count = new int[subjectMax];
+        for (int row = 0; row < 4; row++)
+        {
+            for (int col = 0; col < grades[row].Length - 1; col++)
+            {
+                grades[4][col] += grades[row][col];
+                count[col]++;
+            }
+        }
+        for (int col = 0; col < grades[4].Length; col++)
+        {
+            grades[4][col] /= count[col];
+        }
+
+        //display the array
+        Console.Write("Student/SubjectID \t");
+        for (int i = 1; i <= subjectMax; i++)
+        {
+            Console.Write($"{i}\t");
+        }
+
+        Console.WriteLine("Total");
+
+        for (int row = 0; row < 4; row++)
+        {
+            if (row == 4)
+            {
+                Console.Write("Subject_AVG\t\t");
+            }
+            else
+            {
+                Console.Write($"{row + 1}\t\t\t");
+            }
+            for (int col = 0; col < grades[row].Length - 1; col++)
+            {
+                Console.Write($"{grades[row][col]}\t");
+            }
+            for (int tab = 0; tab < subjectMax - grades[row].Length + 1; tab++)
+            {
+                Console.Write("\t");
+            }
+            Console.WriteLine($"{grades[row][grades[row].Length - 1]}\t");
+        }
+        Console.Write("subjectAVG\t\t");
+        for (int col = 0; col < subjectMax; col++)
+        {
+            Console.Write($"{grades[4][col]}\t");
+        }
+    }
+}
---
+++ C-_miniProjects/multi-dimentional array/Program.cs
@@ -0,0 +1,59 @@
+﻿class myClass
+{
+    public static void Main()
+    {   //declare+create 2D grades array
+        int[,] grades = new int[5, 4];
+        //fill grades
+        int grade = 10;
+        for (int row = 0; row < 4; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                grades[row, col] = grade;
+                grade += 10;
+            }
+        }
+        //calculate total for each student
+
+        for (int row = 0; row < 4; row++)
+        {
+            int total = 0;
+            for (int col = 0; col < 3; col++)
+            {
+                total += grades[row, col];
+            }
+            grades[row, 3] = total;
+        }
+        //calculate AVG for each subject
+        for (int col = 0; col < 3; col++)
+        {
+            int AVG = 0;
+            for (int row = 0; row < 4; row++)
+            {
+                AVG += grades[row, col];
+            }
+            AVG /= 4;
+            grades[4, col] = AVG;
+        }
+        //display the array
+        Console.WriteLine("Student/SubjectID \t 1\t2\t3\tTotal");
+        
+        for (int row = 0; row < 5; row++)
+        {
+            if (row == 4)
+            {
+                Console.Write("Subject_AVG\t\t");
+            }
+            else
+            {
+                Console.Write($"{row + 1}\t\t\t");
+            }
+            for (int col = 0; col < 4; col++)
+            {
+                Console.Write($"{grades[row,col]}\t");
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
