d49388b7e0dcd02be5c68376ebd1f58770b03b03
Merna Magdy <mernamagdy260@gmail.com>
2025-09-30 14:30:15 +0300

    synchronized

--- C-_miniProjects/Windows Programming/ABC_Dashboard/obj/Debug/net8.0-windows/ABC_Dashboard.AssemblyInfo.cs
+++ C-_miniProjects/Windows Programming/ABC_Dashboard/obj/Debug/net8.0-windows/ABC_Dashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABC_Dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b531acaa41795b65d9a38a6c794346c4c91b54")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABC_Dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABC_Dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
